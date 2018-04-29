57d2b8d0ba2cd39ac9ac8782e54f158b87a731a8
Jonatan Hadas <j@VM-6.SEGM40>
2018-04-29 23:03:06 +0300

    Add basic interface for parser

--- parser.h
+++ parser.h
@@ -1,6 +1,19 @@
 #ifndef _PARSER_H
 #define _PARSER_H
+/*
+This module parses input into commands with parameters, and errors.
+*/
 
 
+typedef enum command_t{
+	CMD_SET;
+	CMD_HINT;
+	CMD_VALIDATE;
+	CMD_RESTART;
+	CMD_EXIT;
+	CMD_INVALID; /* used when an invalid command is entered */
+} Command;
+
+Command get_command();
 
 #endif
