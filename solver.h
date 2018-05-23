7e1d50617a8646fc208ea7394cd8cdfc52644a86
Eduard-Gorohovski <37761050+Eduard-Gorohovski@users.noreply.github.com>
2018-05-23 16:24:14 +0300

    Update solver.h

--- solver.h
+++ solver.h
@@ -1,29 +1,17 @@
-#ifndef _SOLVER_H
-#define _SOLVER_H
+
+#ifndef SOLVER_H_
+#define SOLVER_H_
 
 #include "game.h"
 
-/*
-This module provides functions for solving and generating game boards
-*/
+#define MAX_VALUE (game->cell_size)*(game->cell_size)
 
-/*
-Generates a solution to a given board deterministicaly, and returns it
+int brute_solver(Board* game);
 
-if there is no solution returns NULL
-*/
-Solution* solve_det(SudokuGame* game);
+int rand_solver(Board* game);
 
-/*
-Generates a randomized solution to a given board, and returns it
+void set_values(Board* game);
 
-if there is no solution returns NULL
-*/
-Solution* solve_rand(SudokuGame* game);
 
-/*
-Generates a new game with a given number of fixed cells, and a saved solution
-*/
-SudokuGame* generate_game(int num_fixed);
 
-#endif
+#endif /* SOLVER_H_ */
