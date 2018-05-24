3cbe16a2081486c36b91ce8162bf101dcacec4e5
Eduard-Gorohovski <37761050+Eduard-Gorohovski@users.noreply.github.com>
2018-05-24 08:47:27 +0300

    Update game.c

--- game.c
+++ game.c
@@ -1,144 +1,151 @@
 
 
-
-#include "mainAux.h"
-#include "solver.h"
-#include "game.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
+#define MAX_COMMAND_LENGTH 1024
+#define SET_COMMAND 1
+#define HINT_COMMAND 2
+#define VALIDATE_COMMAND 3
+#define RESTART_COMMAND 4
+#define EXIT_COMMAND 5
 
-int brute_solver_rec(Board* game, int i, int j);
-int validate_value(Board* game,int value,int i,int j);
-int get_random_num(int *values, int num_valid);
-int rand_solver_rec(Board* game, int** avilability_table, int i, int j);
+int get_number_from_string(char *input_string);
+int isDigit(char c);
 
 /*
- * ret: 1, such that for every location (i,j) the value is legal. When 1<value<MAX_VALUE, if possible.
- * ret: 0, when no legal solution is possible with current set values.
+ * param max number of fixed cells.
+ * 0<=ret<max_cells when successful
+ * ret =-1 if the function failed
+ *
+ * function read the input from the user if it's a valid number, returns it. If invalid number asks the user again.
+ * If it's another input exits with an eror
  */
-int brute_solver(Board* game){
-	set_values(game);
-	return brute_solver_rec(game,0,0);
+int read_num_fixed_cells(int max_cells){
+	int fixed_cells=0, check=0;
+	while(1){
+		printf("Please enter the number of cells to fill [0-%d]:\n",max_cells-1);
+		fflush(stdout);
+		check=scanf("%d",&fixed_cells);
+		if(check<0){ /* string contains EOF, command is exit */
+			fixed_cells=-1;
+			break;
+		}
+		if(fixed_cells>=0 && fixed_cells<max_cells) break;
+		printf("Error: invalid number of cells to fill (should be between 0 and %d)\n",max_cells-1);
+		fflush(stdout);
+	}
+	return fixed_cells;
 }
 
+
 /*
- * parameters: game is the Sudoku game, (i,j) indicate position on the Sudoku board
- * 0<i,j<game->size
- * function recursively traverses the board. leaves fixed values untouched (marked with a minus).
- * function sweeps the board from left to right and from bottom down
- * Each time the function gets to a non fixed position (i,j), it will run sequentially on numbers 1-MAX_VALUE till it finds a valid input
- * once one valid input is found will proceed to the next cell.
- * When all possible inputs are invalid in current state will backtrack to previous location.
- * At previous location will continue sequentially attempts over possible values.
- *
- * ret: 1, such that for every location (i,j) the value is legal. When 1<value<MAX_VALUE, if possible.
- * ret: 0, when no legal solution is possible with current set values.
+ * Assuming that result =/= NULL and contains space for 4 integers
+ * ret[0] describes the command from user.
+ * 1.set  2.hint  3.validate  4.restart  5.exit
+ * in case of EOF uses exit command
+ * when finished !=0 only commands 4.restart 5.exit are accepted
+ * ret[1]-ret[3] are used for command parameters when needed
  */
-int brute_solver_rec(Board* game, int i, int j){
-	int value=1;
-	if(game->table[i][j]<0){ /* the value is set */;
-		if((i==game->size-1)&&(j==game->size-1)) return 1; /* success! */
-		return(brute_solver_rec(game,i+((j+1)/(game->size)),(j+1)%(game->size))); /* move to the next */
+int* get_command(int *result, int finished){
+	int num_from_string=-1, i=0;
+	char *input_string = malloc(MAX_COMMAND_LENGTH), *check = NULL, *token = NULL, *delimiter=" \t\r\n";
+	if(input_string==NULL){
+		printf("Error: malloc has failed\n");
+		fflush(stdout);
+		*result=EXIT_COMMAND;
+		return result;
 	}
-	else{
-		for(value=1;value<=(MAX_VALUE);value++){
-			if(validate_value(game, value, i, j)){ /* check if value is legal */
-				game->table[i][j]=value;
-				/*printf("(i,j)=(%d,%d), value=%d",i,j,value);*/
-				if((i==game->size-1)&&(j==game->size-1)) return 1; /* success! */
-				if(brute_solver_rec(game,i+((j+1)/(game->size)),(j+1)%(game->size))) return 1; /* check next cell */
+
+	while(1){
+		check = fgets(input_string,MAX_COMMAND_LENGTH,stdin);
+		if(check==NULL){ /* string contains EOF, command is exit */
+			*result=EXIT_COMMAND;
+			free(input_string);
+			return result;
+		}
+		token=strtok(input_string,delimiter);
+		if (token == NULL){ /*input is only white spaces, get new input*/
+			continue;
+		}
+		if(finished==0 && strcmp(token,"set")==0){ /* case command is set */
+			*result=SET_COMMAND;
+			for(i=1;i<=3;i++){  	/* get the cell and value */
+				token = strtok(NULL, delimiter);
+				num_from_string=get_number_from_string(token);
+				if(num_from_string==-1){  /* invalid command */
+					printf("Error: invalid command\n");
+					fflush(stdout);
+					break;
+				}
+				result[i]=num_from_string;
 			}
+			if(num_from_string==-1) continue; /* invalid command - get next one */
+			break;
+		}
+		if(finished==0 && strcmp(token,"hint")==0){ /* case command is hint */
+			*result=HINT_COMMAND;
+			for(i=1;i<=2;i++){ /* get the cell */
+				token = strtok(NULL, delimiter);
+				num_from_string=get_number_from_string(token);
+				if(num_from_string==-1){ /* invalid command */
+					printf("Error: invalid command\n");
+					fflush(stdout);
+					break;
+				}
+				result[i]=num_from_string;
+			}
+			if(num_from_string==-1) continue; /* invalid command - get next one */
+			break;
+		}
+		if(finished==0 && strcmp(token,"validate")==0){  /* case command is validate */
+			*result=VALIDATE_COMMAND;
+			break;
+		}
+		if(strcmp(token,"restart")==0){  /* case command is restart */
+			*result=RESTART_COMMAND;
+			break;
+		}
+		if(strcmp(token,"exit")==0){  /* case command is exit */
+			*result=EXIT_COMMAND;
+			break;
+		}
+		else{
+			printf("Error: invalid command\n");
+			fflush(stdout);
 		}
 	}
-	game->table[i][j]=0; /* no valid values, setting the cell to 0 before going back */
-	return 0;
-}
-
-int rand_solver(Board* game){
-	int i=0, result=0;
-	int* mem_aloc;
-	int** avilability_table;
-	mem_aloc=calloc(MAX_VALUE*(game->size)*(game->size),sizeof(int));
-	avilability_table=calloc((game->size)*(game->size),sizeof(int*));
-	if(!avilability_table || !mem_aloc){ /* allocation failed */
-		printf("Error: calloc has failed\n");
-		return 0;
-	}
-	for(i=0;i<(game->size)*(game->size);i++){
-		avilability_table[i] = mem_aloc+i*MAX_VALUE;
-	}
-	result = rand_solver_rec(game, avilability_table, 0, 0);
-	free(avilability_table);
-	free(mem_aloc);
+	free(input_string);
 	return result;
 }
 
-int rand_solver_rec(Board* game, int** avilability_table, int i, int j){
-	int* values;
-	int k=0, num_valid=0, random_number;
-	values=avilability_table[i*(game->size)+j];
-
-	for(;k<MAX_VALUE;k++){
-		if(validate_value(game, k+1, i, j)){
-			values[k]=1;
-			num_valid++;
-		}
-		else values[k]=0;
+/*
+ * gets an input string.
+ * if string is empty or contains anything but numbers, return -1: Eror
+ * returns the string parsed to an int number
+ */
+int get_number_from_string(char *input_string){
+	long unsigned size=0;
+	int i=0, result=0, decimal=1;
+	if(input_string==NULL) return -1;
+	size= strlen(input_string);
+	if(size==0) return -1;
+	for(i=size-1;i>=0;i--){
+		if(!isDigit((*(input_string+i)))) return -1;
+		result+=(((int)(*input_string+i)-'0'))*(decimal);
+		decimal*=10;
 	}
+	return result;
+}
 
-/*	print_sudoku(game);
-	fflush(stdout); */
+/*
+ * input is a char.
+ * returns 1 if char c contains an integer. 0 otherwise
+ */
 
-	while(num_valid>0){
-/*		printf("(i,j)=(%d,%d)",i+1,j+1);
-		printf("num_valid: %d  values: ",num_valid);
-		for(k=0;k<MAX_VALUE;k++){
-			if(values[k]==1){
-				printf("%d, ", k+1);}}
-		printf("\n");
-		fflush(stdout); */
-		random_number=get_random_num(values, num_valid);
-		num_valid--;
-		game->table[i][j]=random_number;
-		if((i==game->size-1)&&(j==game->size-1)) return 1; /* success! */
-		if(rand_solver_rec(game, avilability_table, i+((j+1)/(game->size)),(j+1)%(game->size))) return 1;
-	}
-	game->table[i][j]=0;
+int isDigit(char c){
+	if(((int)c >= (int)'0')&&((int)c <= (int)'9')) return 1;
 	return 0;
 }
-
-/*
- * negative integers on the board indicate values that are set, and cannot be changed.
- * Fucntion sets all non zero values.
- */
-void set_values(Board* game){
-	int i=0, j=0;
-	for(;i<(game->size);i++){
-		for(j=0;j<(game->size);j++){
-			game->table[i][j]=MIN(game->table[i][j], ((-1)*(game->table[i][j])));
-		}
-	}
-}
-
-
-/*
- * gets an array of possible values. a cell in the array with value 1 indicates that the index+1 of cell is valid.
- * 0 indicate invalid.
- * If there is only one valid num, the function returns it. otherwise it uses rand() to select the index of the random number from all valid ones
- * returns one of the valid values.
- */
-int get_random_num(int *values,int num_valid){
-	int i=0, random_index=0; /* case there is only one valid number set the "random index" to first*/
-	if(num_valid>1){ /* otherwise use random to choose index */
-		random_index=rand()%num_valid;
-	}
-	for(;random_index>=0;i++){
-		if(values[i]==1) random_index--;
-	}
-	values[i-1]=0; /*sets 0 for the num we're about to use */
-	return i;
-}
-
 
